2fc70456fb3df21d901a6f29efd300b9e0dcd74e
Ezra Spier <ezra.spier@gmail.com>
2013-04-23 14:10:55 -0400

    WIP: More filters.

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -11,4 +11,5 @@ class Incident
 
   scope :arrest_borough, ->(county_code) { where("#{ARREST_PREAMBLE}.p:ArrestLocation.p:LocationCountyCode" => county_code) }
   scope :arrest_charges_include, ->(charge_code) { where("#{ARREST_PREAMBLE}.p:ArrestCharge.p:ChargeClassCode" => charge_code) }
+  scope :defendant_sex, ->(sex_code) { where("#{ARREST_PREAMBLE}.p:ArrestSubject.p:Subject.p:PersonPhysicalDetails.p:PersonSexCode" => sex_code) }
 end
