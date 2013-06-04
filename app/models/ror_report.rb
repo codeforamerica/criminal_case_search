ab1f1e974476635bf7f09025e0e3d593413bd340
CJ Bryan <christianbryan@gmail.com>
2013-06-04 15:45:07 -0700

    Prelim. ROR Report display on the homepage.Closes #25.

--- app/models/ror_report.rb
+++ app/models/ror_report.rb
@@ -8,7 +8,14 @@ class RorReport < DatashareDocument
     report["n:Arrest"]["j:ActivityID"]["j:ID"]
   end
 
+  def recommendations
+    # the to_a ensures that this method always returns an array.
+    # singular comments would otherwise be returned as strings.
+    report["c:RORInterview"]["j:ActivityCommentText"].to_a
+  end
+
   private
+
   def report
     body["c:RORInterviewReports"]["c:RORInterviewReport"]
   end
