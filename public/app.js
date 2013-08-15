5a9291ecc7804c93ad7a173e8318199fb68070b7
CJ Bryan <christianbryan@gmail.com>
2013-08-15 12:16:15 -0700

    Move our JS code to its own file.

---
+++ public/app.js
@@ -0,0 +1,23 @@
+$(function(){
+  $('.more-or-less').each(function(i, el) {
+  console.log(el);
+    $(el).find("li a").click(function() {
+    $(el).find("button").html($(this).text() + ' <span class="caret"></span>');
+    $(el).find("input.more-or-less-type").val($(this).text());
+    });
+  });
+
+  $(".btn-group.buttons-radio").each(function(i, el) {
+    $(el).find("button").click(function() {
+      var hidden_field = $(el).find('input[type="hidden"]')
+      hidden_field.val($(this).text());
+    })
+  })
+
+  $('.slider').slider({
+    range: true,
+    min: 18,
+    max: 100,
+    values: [ 18, 65 ]
+  });
+})
