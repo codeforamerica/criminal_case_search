a9df5ed9fee6f4e752634e2b96ddbd2ee6c6fce8
CJ Bryan <christianbryan@gmail.com>
2013-08-18 14:19:33 -0400

    Use a radio button builder and have top charge remeber its state.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
-
-
+  def build_radio_field(name, value, label)
+<<html
+  <div class="field">
+    <input type="radio"
+           name="filter[#{name}]"
+           id="#{name}-#{value}"
+           value="#{value}"
+           #{params[:filter][name] == value ? "checked" : ""}>
+    <label for="#{name}-#{value}">#{label}</label>
+  </div>
+html
+  end
 end
