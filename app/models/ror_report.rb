49a5e0f223ef0a244ddf6d033989aa8cfc5bcb2a
Ezra Spier <ezra.spier@gmail.com>
2013-08-18 17:54:27 -0400

    Refactor ROR Reports.

--- app/models/ror_report.rb
+++ app/models/ror_report.rb
@@ -1,22 +1,24 @@
-require_relative 'datashare_document'
-
-class RorReport < DatashareDocument
+class RorReport
   include Mongoid::Document
   embedded_in :incident
 
-  def arrest_id
-    report["n:Arrest"]["j:ActivityID"]["j:ID"]
-  end
+  field :arrest_id, type: String
+  field :recommendations, type: String
 
-  def recommendations
-    # the to_a ensures that this method always returns an array.
-    # singular comments would otherwise be returned as strings.
-    report["c:RORInterview"]["j:ActivityCommentText"].to_a
-  end
+  def self.from_xml(xml_string)
+    importer = XMLDocImporter.new(xml_string, "/e:EnterpriseDatashareDocument/e:DocumentBody/c:RORInterviewReports/c:RORInterviewReport")
 
-  private
+    ror = RorReport.new
+    ror.arrest_id = importer.attribute_from_xpath("/n:Arrest/j:ActivityID/j:ID")
+    ror.incident = Incident.find_or_initialize_by(arrest_id: ror.arrest_id)
+    recs = importer.attribute_from_xpath("/c:RORInterview/j:ActivityCommentText").to_a
+    ror.recommendations = recs.map do |rec|
+      rec.capitalize
+         .gsub("ror","ROR")
+         .gsub("nysid", "NYSID")
+         .gsub("fta", "FTA")
+    end.join(", ")
 
-  def report
-    body["c:RORInterviewReports"]["c:RORInterviewReport"]
+    [ror]
   end
 end
