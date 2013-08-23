910bf24480ede7986934332dea2b80c580095cbe
Ezra Spier <ezra.spier@gmail.com>
2013-08-23 12:57:55 -0400

    Properly identify VFOs from complaints.

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,12 +1,18 @@
 class Complaint
   include Mongoid::Document
   embedded_in :incident
+  ATTEMPTED_VFOS = %w[120.06 120.07 120.08 120.10 125.25 125.27 135.20
+                      140.25 140.30 150.15 160.10 160.15 265.03]
+  VFOS = %w[120.02 120.05 120.06 120.07 120.08 120.10 120.11 125.15 125.20
+            125.25 125.27 130.53 130.75 130.95 130.96 135.20 140.25 140.30
+            150.15 160.10 160.15 215.16 240.55 240.61 263.16 265.03 265.04
+            265.08 265.09 265.12 265.13]
 
   field :arrest_id, type: String
   validates :arrest_id, presence: true, uniqueness: true
 
   field :top_charge_code, type: String
-  validates :top_charge_code, inclusion: { in: %w(I V M F), allow_nil: true }
+  validates :top_charge_code, inclusion: { in: %w(I V M F VF), allow_nil: true }
 
   field :charges, type: Array
   field :complaint_image, type: Moped::BSON::Binary
@@ -36,25 +42,30 @@ class Complaint
                    agency_code: charge_node.xpath("next:NYCChargeAugmentation/next:ChargeAgencyCode", importer.namespaces).first.content
                  }
 
-        if charge[:agency_code] =~ /PL 220/
+        if charge[:agency_code] =~ /PL (110\/)220/
           c.drug_charge = true
         end
 
-        if charge[:agency_code] =~ /PL 120\.00/
+        if charge[:agency_code] =~ /PL (110\/)120\.00/
           c.misdemeanor_assault_charge = true
         end
 
-        if charge[:agency_code] =~ /PL (215.50|215.51|215.52)/
+        if charge[:agency_code] =~ /PL (110\/)(215.50|215.51|215.52)/
           c.criminal_contempt_charge = true
         end
 
-        if charge[:agency_code] =~ /PL 130/
+        if charge[:agency_code] =~ /PL (110\/)130/
           c.sex_offense_charge = true
         end
 
         charge
       end
-      c.top_charge_code = c.charges.first[:category]
+
+      if Complaint.violent_felony_offense?(c.charges.first)
+        c.top_charge_code = "VF"
+      else
+        c.top_charge_code = c.charges.first[:category]
+      end
 
       #c.complaint_image = importer.attribute_from_xpath("/nc:DocumentBinary/nc:BinaryBase64Object") do |b64|
         #tiff_filename = "/tmp/#{c.arrest_id}.tiff"
@@ -81,6 +92,18 @@ class Complaint
     complaints
   end
 
+  def self.violent_felony_offense?(charge)
+    if charge[:attempted] == "true"
+      ATTEMPTED_VFOS.each do |vfo|
+        return true if charge[:agency_code] =~ /PL (110\/)#{Regexp.quote(vfo)}/i
+      end
+    else
+      VFOS.each do |vfo|
+        return true if charge[:agency_code] =~ /PL #{Regexp.quote(vfo)}/i
+      end
+    end
+    false
+  end
 
   private
   def update_incident_attributes
