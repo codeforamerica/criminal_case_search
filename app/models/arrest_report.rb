5b10cb9cbf1be299079d10f854cadb1975c02fec
Ezra Spier <ezra.spier@gmail.com>
2013-09-04 16:56:14 -0700

    Save court date from Arrest Report if DAT.

--- app/models/arrest_report.rb
+++ app/models/arrest_report.rb
@@ -19,6 +19,7 @@ class ArrestReport
 
   field :precinct, type: String
   field :desk_appearance_ticket, type: Boolean
+  field :desk_appearance_ticket_court_date, type: Date
 
   before_save :update_incident_attributes
 
@@ -35,6 +36,9 @@ class ArrestReport
     ar.defendant_sex = importer.attribute_from_xpath("/p:Arrest/p:ArrestSubject/p:Subject/p:PersonPhysicalDetails/p:PersonSexCode")
     ar.defendant_age = importer.attribute_from_xpath("/p:Arrest/p:ArrestSubject/p:Subject/p:PersonAge")
     ar.desk_appearance_ticket = importer.attribute_from_xpath("/p:DeskAppearanceTicketData/p:DeskAppearanceTicketID/j:ID") { |dat_id| dat_id == "000000000" ? false : true }
+    if ar.desk_appearance_ticket == true
+      ar.desk_appearance_ticket_court_date = Date.parse(importer.attribute_from_xpath("/p:DeskAppearanceTicketData/p:DeskAppearanceInitialCourtDate"))
+    end
 
     ar.incident = Incident.find_or_initialize_by(arrest_id: ar.arrest_id)
 
@@ -48,5 +52,9 @@ class ArrestReport
   private
   def update_incident_attributes
     incident.update_attributes(defendant_sex: defendant_sex, borough: borough, defendant_age: defendant_age)
+    if desk_appearance_ticket?
+      #Todo: which court part?
+      incident.update_attributes(next_court_date: desk_appearance_ticket_court_date)
+    end
   end
 end
