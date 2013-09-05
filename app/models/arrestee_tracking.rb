99cb33202b2dc10537d7049af7cf7fe9db4bfece
Ezra Spier <ezra.spier@gmail.com>
2013-09-04 17:41:19 -0700

    Save arraignment outcomes from arrestee tracking notifications

--- app/models/arrestee_tracking.rb
+++ app/models/arrestee_tracking.rb
@@ -5,21 +5,34 @@ class ArresteeTracking
   field :arrest_id, type: String
   validates :arrest_id, presence: true, uniqueness: true
 
+  field :arraigned, type: Boolean
+  field :arraignment_outcome, type: String
+  field :arraignment_time, type: DateTime
+
+  before_save :update_incident_attributes
+
   def self.from_xml(xml_string)
     importer = XMLDocImporter.new(xml_string, "/soapenv:Envelope/soapenv:Body/ds:ArresteeTrackingXml")
 
     arrestee_trackings = []
     arrest_ids = importer.attribute_from_xpath("/j:Arrest/nc:ActivityIdentification/nc:IdentificationID").compact
 
-    # TODO: There are separate arrest IDS for ARR_ID and LEAD_ARR_ID. Should we choose one instead of both?
     arrest_ids.each do |arrest_id|
       at = ArresteeTracking.find_or_initialize_by(arrest_id: arrest_id)
       at.arrest_id = arrest_id
       at.incident = Incident.find_or_initialize_by(arrest_id: at.arrest_id)
-      arrestee_trackings << at
+      at.arraignment_outcome = importer.attribute_from_xpath("/j:CaseCourtEvent/j:CourtEventAction/nc:ActivityDisposition/nc:DispositionDescriptionText")
+      if at.arraignment_outcome
+        at.arraigned = true
+        at.arraignment_time = DateTime.parse(importer.attribute_from_xpath("/j:CaseCourtEvent/j:CourtEventAction/nc:ActivityDisposition/nc:DispositionDate/nc:DateTime"))
+        arrestee_trackings << at
+      end
     end
-    # TODO: Save other data
-
     arrestee_trackings
+  end
+
+  private
+  def update_incident_attributes
+    incident.update_attributes(arraigned: arraigned)
   end
 end
